8375d12e6c5fe8e9773535dc2fb10abaae00af1e
aparnak22 <aparna.w@gmail.com>
2022-05-18 11:35:02 +0100

    adding Test class for the movie search service

---
+++ src/test/java/com/techreturners/movieapi/service/MovieSearchServiceTest.java
@@ -0,0 +1,86 @@
+package com.techreturners.movieapi.service;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.techreturners.movieapi.model.MovieInfo;
+import com.techreturners.movieapi.model.MovieRecommendations;
+import com.techreturners.movieapi.service.MovieSearchService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.client.ExpectedCount;
+import org.springframework.test.web.client.MockRestServiceServer;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
+
+
+
+@RestClientTest(MovieSearchService.class)
+public class MovieSearchServiceTest {
+
+
+
+    @Autowired
+    private MovieSearchService movieSearchService ;
+
+    @Autowired
+    private MockRestServiceServer mockServer;
+    
+    ObjectMapper mapper;
+
+    @BeforeEach
+    public void setup() {
+        mapper = new ObjectMapper();
+    }
+
+
+    @Test
+    public void testSearchMethodIsCalledt() throws Exception{
+
+        String testURI=
+                "https://api.themoviedb.org/3/discover/movie?api_key=70cb6bbe42e70d00630d60e98da21a75&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=2&with_genres=878&with_watch_monetization_types=flatrate";
+        MovieRecommendations movieRecommendations ;
+        MovieInfo[] movieInfoArr = new MovieInfo[1];
+
+        movieInfoArr[0] = new MovieInfo(399566L, "en", "2021-03-24", "Godzilla vs. Kong",
+                "In a time when monsters walk the Earth, humanity's fight for its future sets Godzilla and Kong " +
+                        "on a collision course that will see the two most powerful forces of nature on the planet collide " +
+                        "in a spectacular battle for the ages.", 7.0F);
+
+         movieRecommendations =  new MovieRecommendations(
+                        "1",
+                         movieInfoArr,
+                        747L,
+                        14938L);
+
+        mockServer.expect(ExpectedCount.once(),
+                        requestTo(testURI))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withStatus(HttpStatus.OK)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(mapper.writeValueAsString(movieRecommendations))
+                );
+
+        MovieRecommendations actual = movieSearchService.getRecommendations();
+        assertEquals(movieRecommendations.getTotal_results(), actual.getTotal_results());
+        assertEquals(movieRecommendations.getResults()[0].getTitle(), actual.getResults()[0].getTitle());
+        mockServer.verify();
+    }
+}
